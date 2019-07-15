831b8b570089e09486e92e71dee8ab11eec689a4
mcj <1585918524@qq.com>
2019-07-15 19:51:49 +0800

    修复上传图片文件bug

---
+++ minmeng-manager/src/main/java/com/minmeng/manager/MyInit.java
@@ -0,0 +1,17 @@
+/*
+package com.minmeng.manager;
+
+import com.j2mvc.framework.J2mvcListener;
+import com.j2mvc.framework.dao.JdbcDaoSupport;
+
+*/
+/**
+ * @author John
+ * @decription m
+ * @date 2019/7/8
+ *//*
+
+public class MyInit extends JdbcDaoSupport {
+
+
+}*/
